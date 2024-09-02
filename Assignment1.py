a6341e112dc1ea7f23ef1704f3bb6fc1509ed225
Avinna07 <abhattarai28547@ucumberlands.edu>
2024-09-01 20:26:50 -0500

    Book example

--- Assignment1.py
+++ Assignment1.py
@@ -1,6 +1,14 @@
 #Avinna Bhattarai 
-#this is the created file to upload and check if pythone code is being executed
+#Using the algorithm from the book
+A= [7,8,5,4,3]
+n = len(A)
+for i in range(1, n):
+        key = A[i]
+        j = i - 1
+        # Move elements of A[0..i-1], that are greater than key, to one position ahead of their current position
+        while j >= 0 and A[j] > key:
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = key
 
-print("Hello this is a test")
-
-#executed sucessfully
+print(A)
