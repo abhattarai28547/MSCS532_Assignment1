0aa8ec466aac4f0facba208744ce6cb96a7defbc
Avinna07 <abhattarai28547@ucumberlands.edu>
2024-09-01 21:20:10 -0500

    Final Commit

--- Assignment1.py
+++ Assignment1.py
@@ -1,19 +1,25 @@
 #Avinna Bhattarai 
 #Write a Python program for the Insertion Sort Algorithm that sorts in monotonically decreasing order.
 
-#This is an example array hardcoded for this program
-A= [7,8,5,4,3]
+#Method for Decreasing Insertion Sort
 
-#setting n as number of elements in the given array
-n = len(A)
+def insertion_sort_decreasing(A):
+    #setting n as number of elements in the given array
+    n = len(A)
 
-for i in range(1, n):
-        key = A[i]
-        j = i - 1
-        # Moving elements of A[0..i-1], that are greater than key, to one position ahead of their current position
-        while j >= 0 and A[j] < key:
-            A[j + 1] = A[j]
-            j -= 1
-        A[j + 1] = key
+    for i in range(1, n):
+            key = A[i]
+            j = i - 1
+            # Moving elements of A[0..i-1], that are greater than key, to one position ahead of their current position
+            while j >= 0 and A[j] < key:
+                A[j + 1] = A[j]
+                j -= 1
+            A[j + 1] = key
 
-print(A)
+
+#main 
+ #This is an example array(The numerical elements can be changed as desired)
+A= [56,46,77,23,99]
+print("Given array: ",A)
+insertion_sort_decreasing(A)
+print("Sorted array in decreasing order: ",A)
