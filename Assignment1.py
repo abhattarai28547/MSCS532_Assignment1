f0b608e4af9b7485843e8bb0e86110e8f82dda12
Avinna07 <abhattarai28547@ucumberlands.edu>
2024-09-01 21:12:04 -0500

    Decreasing Order

--- Assignment1.py
+++ Assignment1.py
@@ -1,12 +1,17 @@
 #Avinna Bhattarai 
-#Using the algorithm from the book
+#Write a Python program for the Insertion Sort Algorithm that sorts in monotonically decreasing order.
+
+#This is an example array hardcoded for this program
 A= [7,8,5,4,3]
+
+#setting n as number of elements in the given array
 n = len(A)
+
 for i in range(1, n):
         key = A[i]
         j = i - 1
-        # Move elements of A[0..i-1], that are greater than key, to one position ahead of their current position
-        while j >= 0 and A[j] > key:
+        # Moving elements of A[0..i-1], that are greater than key, to one position ahead of their current position
+        while j >= 0 and A[j] < key:
             A[j + 1] = A[j]
             j -= 1
         A[j + 1] = key
